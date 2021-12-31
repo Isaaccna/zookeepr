8074122b2a953db7cfee134a26335c573749fe22
Isaaccna <isaac.andrade1231@gmail.com>
2021-12-31 18:40:03 -0500

    front end linked to the server, addind data by submitting the form added

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 const fs = require('fs');
 const path = require('path');
 
@@ -113,6 +115,22 @@ function filterByQuery(query, animalsArray) {
   }
   });
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
